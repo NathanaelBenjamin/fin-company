e547260bf0fb7ab352ca860d8e9c18219177d1a2
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-05-19 12:58:20 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Client from './components/Dashboard/Client/Client'
-import Product from './components/Dashboard/Product/Product'
+
 
 const App = () => {
   return (
