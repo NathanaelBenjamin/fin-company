06542f067981a1cad3703421d670e89c58c8dcd6
JOAN <nobeijoan1.com>
2022-05-25 15:26:18 +0100

    Revert "'firestore'"
    This reverts commit bdffe62f97d969da0ee3f088d494942a09950bd3.

--- src/Pages/AdminDashBoard/AdminDashBoard.js
+++ src/Pages/AdminDashBoard/AdminDashBoard.js
@@ -1,138 +1,136 @@
 import React from 'react'
 import styled from 'styled-components';
 import './AdminDashBoard.css'
-import {
-    AdminDashBoardContainer,
-    linkStyle
-}
-    from './AdminDashBoard.css';
-import { Routes, Route, Navigate, useLocation, Link } from "react-router-dom";
+import { AdminDashBoardContainer,
+    linkStyle} 
+from './AdminDashBoard.css';
+import {Routes, Route, Navigate, useLocation, Link} from "react-router-dom";
 import { useEffect, useState } from "react";
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import Logout from './images/logout.svg';
 import Menu from './images/menu.svg';
-import Dashboard from '../Dashboard/Dashboard';
+import  Dashboard  from '../Dashboard/Dashboard';
 import Client from '../../components/Dashboard/Client/Client'
 import Product from '../../components/Dashboard/Product/Product'
 import TeamMembers from '../../components/Dashboard/Team/Team'
 
-function AdminDashBoard() {
+function AdminDashBoard () {
     // const [click, setClick]=useState(false)
     // const handleClick =()=> setClick(!click)
 
-    const location = useLocation().pathname;
-    const [path, setPath] = useState("");
+  const location = useLocation().pathname;
+  const [path, setPath] = useState("");
+  
+  const openNav = () => {
+      document.getElementById("sidebar").style.left = "0px";
+  }
+  
+  const closeNav = () => {
+      document.getElementById("sidebar").style.left = "-450px";
+  }
 
-    const openNav = () => {
-        document.getElementById("sidebar").style.left = "0px";
-    }
+  const changePathAndClose = (path) => {
+      if(window.innerWidth<=992){
+          closeNav()
+      }
+      setPath(path);
+      
+  }
 
-    const closeNav = () => {
-        document.getElementById("sidebar").style.left = "-450px";
-    }
+  useEffect(()=>{
+      setPath(location);
+     
+  }, [location])
+ 
+  let userDetails = JSON.parse(sessionStorage.getItem("userDetails"))
 
-    const changePathAndClose = (path) => {
-        if (window.innerWidth <= 992) {
-            closeNav()
-        }
-        setPath(path);
-
-    }
-
-    useEffect(() => {
-        setPath(location);
-
-    }, [location])
-
-    let userDetails = JSON.parse(sessionStorage.getItem("userDetails"))
-
-    return (
-        <AdminDashBoardContainer>
-            <div className='sideBar' id='sidebar'
-            // onClick={handleClick} click={click}
-            >
-                <div className='dashBoardTop' >
-                    {/* <p className='dashWelcome'>Welcome, <span className='colorYellow'>{user.displayName}</span></p> */}
-                    <Link style={linkStyle} to="/dashboard">
-                        <div
-                            className={path.includes("dashboard") || path === "dashboard"
-                                ? "dashboardWrapperactive"
-                                : "dashboardWrapper"}
-                            onClick={() => changePathAndClose("dashboard")}
+  return (
+    <AdminDashBoardContainer>
+        <div className='sideBar' id='sidebar' 
+        // onClick={handleClick} click={click}
+        >
+            <div className='dashBoardTop' >
+                <p className='dashWelcome'>Welcome, <span className='colorYellow'>{userDetails.displayName}</span></p>
+                <Link style={linkStyle} to="/dashboard">
+                    <div 
+                        className={path.includes("dashboard") || path==="dashboard"
+                            ?"dashboardWrapperactive"
+                            :"dashboardWrapper"}
+                        onClick={()=>changePathAndClose("dashboard") }
                         >
-                            <AiIcons.AiFillHome />
-                            <p className='dashPara'> Dashboard</p>
-                        </div>
-                    </Link>
-                    <Link style={linkStyle} to="/client">
-                        <div
-                            className={path.includes("client") || path === "client"
-                                ? "clientContaineractive"
-                                : "clientContainer"}
-                            onClick={() => changePathAndClose("client")}
-                        >
-                            <IoIcons.IoIosPaper />
-                            <p className='dashPara'>Our Clients</p>
-                        </div>
-                    </Link>
-                    <Link style={linkStyle} to="/team">
-                        <div
-                            className={path.includes("team") || path === "team"
-                                ? "teamsContaineractive"
-                                : "teamsContainer"}
-                            onClick={() => changePathAndClose("team")}
-                        >
-                            <IoIcons.IoMdPeople />
-                            <p className="dashPara"> Team Members</p>
-                        </div>
-                    </Link>
-                    <Link style={linkStyle} to="/product">
-                        <div
-                            className={path.includes("product") || path === "product"
-                                ? "productContaineractive"
-                                : "productContainer"}
-                            onClick={() => changePathAndClose("product")}
-                        >
-                            <FaIcons.FaCartPlus />
-                            <p className='dashPara'> Products</p>
-                        </div>
-                    </Link>
-                </div>
-                <Link to='/auth' style={linkStyle}>
-                    <div className='logOutWrapper'>
-                        <img src={Logout} alt="icon" />
-                        <p className='dashPara'>Logout</p>
+                        <AiIcons.AiFillHome/>
+                        <p className='dashPara'> Dashboard</p>
                     </div>
                 </Link>
+                <Link style={linkStyle} to="/client">
+                    <div 
+                        className={path.includes("client") || path==="client"
+                            ?"clientContaineractive"
+                            :"clientContainer"}
+                        onClick={()=>changePathAndClose("client") }
+                        >
+                        <IoIcons.IoIosPaper/>
+                        <p className='dashPara'>Our Clients</p>
+                    </div>
+            </Link>
+            <Link style={linkStyle} to="/team">
+                <div 
+                    className={path.includes("team") || path==="team"
+                        ?"teamsContaineractive"
+                        :"teamsContainer"}
+                    onClick={()=>changePathAndClose("team") }
+                    >
+                    <IoIcons.IoMdPeople/>
+                    <p className="dashPara"> Team Members</p>
+                </div>
+            </Link>
+            <Link style={linkStyle} to="/product">
+                <div 
+                    className={path.includes("product") || path==="product"
+                        ?"productContaineractive"
+                        :"productContainer"}
+                    onClick={()=>changePathAndClose("product") }
+                    >
+                    <FaIcons.FaCartPlus/>
+                    <p className='dashPara'> Products</p>
+                </div>
+            </Link>
             </div>
-            <div>
-                {/* <div className='mobileIcons' onclick={handleClick}>
+            <Link to ='/auth' style={linkStyle}>
+                <div className='logOutWrapper'>
+                    <img src={Logout} alt="icon" />
+                    <p className='dashPara'>Logout</p>
+                </div>
+            </Link>
+        </div>
+        <div>
+          {/* <div className='mobileIcons' onclick={handleClick}>
               {click?<FaIcons.FaTimes/> :<FaIcons.FaBars/>}
           </div> */}
-                <Container>
-                    <div className="mobileNav">
-                        <img src={Menu} alt="menu" className="bugger"
-                            onClick={() => openNav()} />
-                        {/* <p className="mobileHead">Managee</p>  */}
-                    </div>
-                </Container>
+            <Container>
+                <div className="mobileNav">
+                    <img src={Menu} alt="menu"  className="bugger" 
+                        onClick={()=>openNav()}/>
+                    {/* <p className="mobileHead">Managee</p>  */}
+                </div>   
+            </Container> 
                 <div className="rooo">
                     <Routes>
-                        <Route path="/" element={<Navigate to="/dashboard" />} />
+                        <Route path="/" element={<Navigate to="/dashboard" />}/>
                         <Route path="/dashboard" element={<Dashboard />} />
                         <Route path="/client" element={<Client />} />
                         <Route path="/product" element={<Product />} />
-                        <Route path="/team" element={<TeamMembers />} />
+                        <Route path="/team" element={<TeamMembers/>} />
                     </Routes>
                 </div>
-            </div>
-        </AdminDashBoardContainer>
-    )
+        </div>
+    </AdminDashBoardContainer>
+  )
 }
 export default AdminDashBoard
 
-const Container = styled.div`
+const Container=styled.div`
 
 `
--- src/components/Authentication/SignUp.js
+++ src/components/Authentication/SignUp.js
@@ -1,75 +1,67 @@
-import React, { useState, } from 'react';
-import { createUserDocumentFromAuth, createEmailAndPasswordAuth } from '../../utils/firebase/firebase.utils';
-import FormInput from '../form-input/formInput';
 
-const defaultformfield = {
-  displayName: '',
-  Email: '',
-  password: '',
-  confirmPassword: ''
-}
-function Signupform() {
-  const [Formfield, setFormfield] = useState(defaultformfield);
-  const { displayName, email, password, confirmPassword } = Formfield;
-
-
-  const handlechange = (event) => {
-    const { name, value } = event.target
-
-    setFormfield({ ...Formfield, [name]: value })
-  };
-
-  console.log(Formfield);
+import React, { useState } from "react";
+import './auth.css'
 
 
 
-  const resetformfields = () => (
-    setFormfield(defaultformfield))
+function SignUp() {
+    const defaultformfield = {
+        displayName: '',
+        email: '',
+        country: '',
+        password: '',
+        confirmpassword: '',
 
-
-  const handleSubmit = (async (e) => {
-    e.preventDefault();
-    if (password != confirmPassword) {
-      alert('password and confirm password do not match')
-    } else {
-      window.location = '/dashboard'
-        ;
     }
-    try {
-      const { user } = await createEmailAndPasswordAuth(email, password);
-      console.log(user);
-      await createUserDocumentFromAuth(user, { displayName })
+    const [formfield, setformfield] = useState(defaultformfield)
+    const { displayName, email, country, password, confirmpassword } = formfield
 
-      resetformfields()
-      window.location = '/dashoard'
-    } catch (error) {
-      if (error.code === 'auth/email-already-in-use') {
-        alert('email already in use');
-      } else if (error.code === 'auth/network-request-failed') {
-        alert('check network connection');
-      } else {
-        console.log('user creation not complete', error);
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-      }
+        setformfield({ ...formfield, [name]: value })
     }
 
-  });
+    const submitHandler = (e) => {
+        e.preventDefault();
 
-  return (
-    <div>
-      <h2>Dont have an account?</h2>
+        sessionStorage.setItem('userDetails', JSON.stringify({ ...formfield }))
 
-      <span>Sign Up with your email and password</span>
-      <form onSubmit={handleSubmit}>
-        <FormInput
-          label='Name' type='text' required onChange={handlechange} name='displayName' value={displayName} />
-        <FormInput label='Email' type="email" required onChange={handlechange} name='email' value={email} />
-        <FormInput label='Password' type='password' required onChange={handlechange} name='password' value={password} />
-        <FormInput label='Confirmpassword' type='password' required onChange={handlechange} name='confirmPassword' value={confirmPassword} />
-        <button type='submit'>Submit</button>
-      </form>
-    </div>
-  );
+        if (password !== confirmpassword) {
+            alert('Passwords do not match')
+        } else {
+            window.location = '/dashboard'
+        }
+
+
+
+    }
+
+    return (
+
+        <div className="forms">
+
+            <form onSubmit={submitHandler}>
+                <h1>Don't Have An Account? Sign Up</h1>
+                <label>Full Name</label>
+                <input type='text' onChange={handleChange} name='displayName' value={displayName} /><br />
+                <label>Email</label>
+                <input type="email" required onChange={handleChange} name='email' value={email} /><br />
+                <label>Country </label>
+                <input type="text" required onChange={handleChange} name='country' value={country} /><br />
+                <label>Password</label>
+                <input type='password' onChange={handleChange} name='password' value={password} /><br /><br />
+                <label>Confirm Password</label>
+                <input type='password' onChange={handleChange} name='confirmpassword' value={confirmpassword} />
+                <div style={{ display: "flex", alignItems: "flex-start" }}>
+                    <input type="checkbox" style={{ width: "25px", margin: "-7px 7px 0 0" }} id="readTerms" />
+                    <p >I have read, understood and i agree to the terms and conditions</p>
+                </div>
+                <button type='submit'>Click Here To Sign Up</button>
+
+            </form>
+        </div>
+    );
 }
 
-export default Signupform;
+export default SignUp;
